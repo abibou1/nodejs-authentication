f7090b5c5a9e5826afbd766526b7ca26983c6004
abibou1 <abibou2017@gmail.com>
2022-02-05 18:00:38 -0600

    fixed linting errors

--- routes/helloRoute.js
+++ routes/helloRoute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const controller = require('../controllers/helloController')
 
-const bodyParser = require ('body-parser')
+const bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
 
 router.get('/hello', controller.helloWorld)
 
-module.exports = router;
+module.exports = router
