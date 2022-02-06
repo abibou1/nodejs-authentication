1061fe8992c164297adced8f50e1b01623c19236
abibou1 <abibou2017@gmail.com>
2022-02-06 09:30:34 -0600

    added typescript package and user db model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,14 @@
 const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body
+
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ error: 'All input are required' })
+    }
+  } catch (e) {
+    return res.status(500).json({ error: e.message })
+  }
+
   res.status(201).json({ token: 'yourtokenhere' })
 }
 
---
+++ model/userModel.js
@@ -0,0 +1,38 @@
+// const mongoose = require('mongoose')
+// const validate = require('mongoose-validator')
+
+// const nameValidator = [
+//   validate({
+//     validator: 'isLength',
+//     arguments: [2, 20],
+//     message: 'Name should contain 2 to 20 characters'
+//   }),
+//   validate({
+//     validator: 'isAlphanumeric',
+//     message: 'Name should contain alpha-numeric characters only'
+//   })
+// ]
+
+// const emailValidator = [
+//   validate({
+//     validator: 'isEmail',
+//     message: 'Invalid email'
+//   })
+// ]
+
+// const passwordValidator = [
+//   validate({
+//     validator: 'isLength',
+//     arguments: [6],
+//     message: 'Password must be at least 6 characters'
+//   })
+// ]
+
+// const userSchema = new mongoose.Schema({
+//   firstName: { type: String, validate: nameValidator },
+//   lastName: { type: String, validate: nameValidator },
+//   email: { type: String, validate: emailValidator },
+//   password: { type: String, required: true, validate: passwordValidator }
+// })
+
+// module.exports = mongoose.model('user', userSchema)
