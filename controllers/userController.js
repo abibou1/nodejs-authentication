58a10989cd2b4a4e67e2d58fe2b5ed2785534095
abibou1 <abibou2017@gmail.com>
2022-02-08 13:11:06 -0600

    created register api(to be improved)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
+const User = require('../model/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body
@@ -5,11 +9,38 @@ const register = async (req, res) => {
     if (!firstName || !lastName || !email || !password) {
       return res.status(400).json({ error: 'All input are required' })
     }
+
+    const existingUser = await User.findOne({ email: email.toLowerCase() })
+
+    if (existingUser) {
+      return res.status(409).json({ error: 'User Already Exist. Please Login' })
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ error: 'Password must be at least 6 characters' })
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10)
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email: email.toLowerCase(),
+      password: encryptedPassword
+    })
+
+    const token = jwt.sign(
+      { email: user.email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: '2h'
+      }
+    )
+
+    return res.status(201).json({ token })
   } catch (e) {
     return res.status(500).json({ error: e.message })
   }
-
-  res.status(201).json({ token: 'yourtokenhere' })
 }
 
 module.exports = {
