11b66cd1ee246201f1d7d7cc726b135f1a4a3190
abibou1 <abibou2017@gmail.com>
2022-02-09 06:23:59 -0600

    improving register

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,38 +6,41 @@ const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body
 
+    const emailLowerCase = email.toLowerCase()
+
     if (!firstName || !lastName || !email || !password) {
       return res.status(400).json({ error: 'All input are required' })
-    }
-
-    const existingUser = await User.findOne({ email: email.toLowerCase() })
-
-    if (existingUser) {
-      return res.status(409).json({ error: 'User Already Exist. Please Login' })
     }
 
     if (password.length < 6) {
       return res.status(400).json({ error: 'Password must be at least 6 characters' })
     }
 
+    const existingUser = await User.findOne({ email: emailLowerCase })
+
+    if (existingUser) {
+      return res.status(409).json({ error: 'User Already Exist. Please Login' })
+    }
+
     const encryptedPassword = await bcrypt.hash(password, 10)
 
-    const user = await User.create({
-      firstName,
-      lastName,
-      email: email.toLowerCase(),
-      password: encryptedPassword
-    })
-
     const token = jwt.sign(
-      { email: user.email },
+      { email: emailLowerCase },
       process.env.TOKEN_KEY,
       {
         expiresIn: '2h'
       }
     )
 
-    return res.status(201).json({ token })
+    await User.create({
+      firstName,
+      lastName,
+      email: emailLowerCase,
+      password: encryptedPassword
+    })
+      .then(() => {
+        return res.status(201).json({ token })
+      })
   } catch (e) {
     return res.status(500).json({ error: e.message })
   }
