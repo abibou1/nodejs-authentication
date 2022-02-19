80c81a8d9045b27432f424c0de6d9869a4f84b94
abibou1 <abibou2017@gmail.com>
2022-02-19 09:10:22 -0600

    complete validation using middleware

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -3,8 +3,26 @@ const { body, validationResult } = require('express-validator')
 
 const userValidationRules = () => {
   return [
-    body('firstName', 'firstName doesn\'t exists').exists(),
-    body('lastName', 'lastName doesn\'t exists').exists()
+    body('firstName')
+      .exists()
+      .withMessage('firstName is required')
+      .isLength({ min: 2, max: 20 })
+      .withMessage('FirstName should contain 2 to 20 characters'),
+    body('lastName')
+      .exists()
+      .withMessage('lastName is required')
+      .isLength({ min: 2, max: 20 })
+      .withMessage('lastName should contain 2 to 20 characters'),
+    body('email')
+      .exists()
+      .withMessage('email is required')
+      .isEmail()
+      .withMessage('Invalid Email'),
+    body('password')
+      .exists()
+      .withMessage('password is required')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters')
   ]
 }
 const validate = (req, res, next) => {
@@ -15,7 +33,7 @@ const validate = (req, res, next) => {
   }
 
   const extractedErrors = []
-  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
+  errors.array({ onlyFirstError: true }).map(err => extractedErrors.push({ [err.param]: err.msg }))
 
   return res.status(422).json({ errors: extractedErrors })
 }
