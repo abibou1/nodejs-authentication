a4cacd1cf21587c1a855b9ac9d608a447c748f6c
abibou1 <abibou2017@gmail.com>
2022-02-26 10:52:37 -0600

    added user routes

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require('express')
-// const expressValidator = require('express-validator')
-const helloController = require('./controllers/helloController')
-const userController = require('./controllers/userController')
-const userValidation = require('./middlewares/userValidation')
 
 require('dotenv').config()
 require('./config/database').connect()
-const apiUrl = '/api/'
 
 const app = express()
 const router = require('./routes/helloRoute')
+const userRouter = require('./routes/userRoute')
 
 app.use(express.json())
 app.use('/', router)
-
-app.get('/hello', helloController.helloWorld)
-app.post(apiUrl + 'register', userValidation.userValidationRules(), userValidation.validate, userController.register)
+app.use('/', userRouter)
 
 module.exports = app
