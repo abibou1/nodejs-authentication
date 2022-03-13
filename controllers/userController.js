8c1f3ac658e8bca9f4bc9be47120c031751bd10f
abibou1 <abibou2017@gmail.com>
2022-03-13 07:43:52 -0500

    handled email duplication in catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,39 +3,36 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body
+  const { firstName, lastName, email, password } = req.body
 
-    const emailLowerCase = email.toLowerCase()
+  const emailLowerCase = email.toLowerCase()
 
-    const existingUser = await User.findOne({ email: emailLowerCase })
+  const encryptedPassword = await bcrypt.hash(password, 10)
 
-    if (existingUser) {
-      return res.status(409).json({ error: 'User Already Exist. Please Login' })
+  const token = jwt.sign(
+    { email: emailLowerCase },
+    process.env.TOKEN_KEY,
+    {
+      expiresIn: '2h'
     }
+  )
 
-    const encryptedPassword = await bcrypt.hash(password, 10)
-
-    const token = jwt.sign(
-      { email: emailLowerCase },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: '2h'
-      }
-    )
-
-    await User.create({
-      firstName,
-      lastName,
-      email: emailLowerCase,
-      password: encryptedPassword
+  await User.create({
+    firstName,
+    lastName,
+    email: emailLowerCase,
+    password: encryptedPassword
+  })
+    .then(() => {
+      return res.status(201).json({ token })
     })
-      .then(() => {
-        return res.status(201).json({ token })
-      })
-  } catch (e) {
-    return res.status(500).json({ error: e.message })
-  }
+    .catch(err => {
+      if (err.keyValue && err.keyValue.email) {
+        return res.status(409).json({ error: 'User Already Exist. Please Login' })
+      } else {
+        return res.status(500).json({ error: err.message })
+      }
+    })
 }
 
 module.exports = {
--- model/userModel.js
+++ model/userModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+  email: { type: String },
+  password: { type: String }
 })
 
 module.exports = mongoose.model('user', userSchema)
